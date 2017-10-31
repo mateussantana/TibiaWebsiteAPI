f02716c187a9a9a531925fbd36e4cbc391594583
Mateus Santana <mateusssantana@gmail.com>
2017-10-31 18:33:19 -0200

    Listagem de servidores

--- example/servidores.php
+++ example/servidores.php
@@ -11,14 +11,9 @@ $dom->preserveWhiteSpace = false;
 $tblRows = $dom->getElementById('worlds')
     ->getElementsByTagName('table')->item(3);
 
-echo "tagName: " . $tblRows->tagName . PHP_EOL;
-echo "class: " . $tblRows->getAttribute('class') . PHP_EOL;
-echo "childs: " . $tblRows->childNodes->length . PHP_EOL;
-//exit;
-
 foreach ($tblRows->childNodes as $row) {
     foreach ($row->childNodes as $td) {
-        echo $td->nodeValue . "\t";
+        echo $td->textContent. "\t";
     }
     echo PHP_EOL;
 }
